5ef9dda1637952d04df5c5ab3c95813db5d7bd5e
mao <maoyuan@gmail.com>
2020-03-05 17:39:56 -0500

    sample csv to mongodb batch job works

--- covid19api/src/main/java/com/maotion/covid19api/Covid19apiApplication.java
+++ covid19api/src/main/java/com/maotion/covid19api/Covid19apiApplication.java
@@ -1,9 +1,11 @@
 package com.maotion.covid19api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Covid19apiApplication {
 
     public static void main(String[] args) {
---
+++ covid19api/src/main/java/com/maotion/covid19api/entities/Domain.java
@@ -0,0 +1,9 @@
+package com.maotion.covid19api.entities;
+
+import lombok.Data;
+
+@Data
+public class Domain {
+    private int id;
+    private String name;
+}
